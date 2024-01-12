8ef42a8a1573de5fd0df93a03e3a3142520a8e17
syslabtech <134613532+syslabtech@users.noreply.github.com>
2024-01-12 07:04:51 +0000

    Add MiniIO Files

---
+++ foldercreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create the folders
+mkdir minio1 minio2 minio3 minio4
+
+# Assign 777 permissions to each folder
+chmod 777 minio1 minio2 minio3 minio4
+
+echo "Folders created and permissions set successfully!"
